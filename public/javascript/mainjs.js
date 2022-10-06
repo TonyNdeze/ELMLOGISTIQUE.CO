fadd7cc0e5841de95f80f584eab38f5da4a64927
Tony Ndeze <113143729+TonyNdeze@users.noreply.github.com>
2022-10-06 12:11:55 +0300

    FirstCommit

--- public/javascript/mainjs.js
+++ public/javascript/mainjs.js
@@ -1,57 +1,32 @@
     // When the event DOMContentLoaded occurs, it is safe to access the DOM
-document.addEventListener('DOMContentLoaded', function() {
-
-    // When the user scrolls the page, execute myFunction 
-  window.addEventListener('scroll', myStickyNav);
-// Get the navbar
-var Navbar = document.getElementById("Navbar");
-
-// Get the offset position of the navbar
-var sticky = Navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myStickyNav() {
-  if (window.pageYOffset >= sticky) {
-    Navbar.classList.add("sticky")
-  } else {
-    Navbar.classList.remove("sticky");
-  }
-}
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myStickyNav()};
-
-
-
-// Reveal content block
-window.addEventListener('scroll', reveal);
-function reveal(){
-  var reveals = document.querySelectorAll(".reveal");
-
-  for(var i = 0; i < reveals.length; i++){
-    
-    var windowheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 50;
-
-    if(revealtop < windowheight - revealpoint){
-     reveals[i].classList.add('active');
-    }
-    else{
-      reveals[i].classList.remove('active');
+    document.addEventListener('DOMContentLoaded', function() {
+      // When the user scrolls the page, execute myFunction 
+    window.addEventListener('scroll', shrinkStick);
+  
+  // Get the offset position of the navbar
+  
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function shrinkStick() {
+    var topBar = document.getElementById("TopBar");
+    var topLogo = document.getElementById("TopBarLogo");
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+      topLogo.style.width = "5em";
+      
+    }else{
+      topLogo.style.width = "8em";
     }
   }
+  // When the user scrolls the page, execute myFunction
+  window.onscroll = function() {shrinkStick()};
+  });
+
+
+
+function toggleNavMenu(x){
+  var LowerBar = document.getElementById("LowerBar");
+  x.classList.toggle("change");
+  LowerBar.classList.toggle("change")
 }
-
-//Reval & Hide Sections
-  
-
-
-});
-  
-
-  
-
-  
 
 
 
